5c2118b43e02e58741861dc6f44fda81c7cf4829
Henrijs Kravals <henrijskravals@gmail.com>
2025-07-09 11:39:52 +0300

    feat: add Lombok support for DTOs

--- src/main/java/com/kenryhraval/banking/dto/LoginRequest.java
+++ src/main/java/com/kenryhraval/banking/dto/LoginRequest.java
@@ -1,12 +1,9 @@
 package com.kenryhraval.banking.dto;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
     private String username;
     private String password;
-
-    // Getters and setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
--- src/main/java/com/kenryhraval/banking/dto/TransferRequest.java
+++ src/main/java/com/kenryhraval/banking/dto/TransferRequest.java
@@ -1,32 +1,10 @@
 package com.kenryhraval.banking.dto;
 
+import lombok.Data;
+
+@Data
 public class TransferRequest {
     private long sourceAccountId;
     private long destinationAccountId;
     private double amount;
-
-    public long getSourceAccountId() {
-        return sourceAccountId;
-    }
-
-    public void setSourceAccountId(long sourceAccountId) {
-        this.sourceAccountId = sourceAccountId;
-    }
-
-    public long getDestinationAccountId() {
-        return destinationAccountId;
-    }
-
-    public void setDestinationAccountId(long destinationAccountId) {
-        this.destinationAccountId = destinationAccountId;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 }
-
